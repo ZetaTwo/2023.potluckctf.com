d58db392af86c5e92e3fe0aea35631bbf64250b2
Calle Svensson <calle.svensson@zeta-two.com>
2023-12-31 15:55:07 +0100

    Change to createWebHashHistory to work better with GH pages

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import homeview from '@/views/HomeView.vue'
 import challengesview from '@/views/ChallengesView.vue'
 import teamsview from '@/views/TeamsView.vue'
@@ -74,7 +74,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes,
 })
 
